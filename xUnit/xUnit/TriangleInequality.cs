81c0d2b8648a7a1ea26fb3d5fe0ce36392a9e99b
gelliz <47990638+gelliz@users.noreply.github.com>
2019-10-07 20:30:57 +0300

    Update TriangleInequality.cs

--- xUnit/xUnit/TriangleInequality.cs
+++ xUnit/xUnit/TriangleInequality.cs
@@ -6,13 +6,11 @@ using System.Threading.Tasks;
 
 namespace xUnit
 {
-    public class triangle
+    public class Triangle
     {
         public static bool IsTriangle(double a, double b, double c)
         {
-            if (a > 0 || b > 0 || c > 0 || a + b > c || a + c > b || b + c > a)
-                return true;
-            return false;
+            return (a > 0 && b > 0 && c > 0 && (a + b > c) && (a + c > b) && (b + c > a));
         }
     }
 }
